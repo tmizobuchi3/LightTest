5e913ba9a7e4a897720aa2491aad941613c440e6
tmizobuchi3 <83824309+tmizobuchi3@users.noreply.github.com>
2021-08-02 22:47:15 +0900

    Initial commit

---
+++ Assets/Generator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject tamaPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shoot");
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject tama = Instantiate(tamaPrefab, new Vector3(0, 3, 0), Quaternion.identity);
+        tama.GetComponent<Rigidbody>().AddForce(Vector3.up);
+    }
+
+}
+
